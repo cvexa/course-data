210d87a854e6f28e08b81a7b891fb14ab47319ee
cvexa <cvexxxxa@abv.bg>
2016-01-27 18:40:40 +0200

    test2

--- probba2.php
+++ probba2.php
@@ -29,34 +29,6 @@
 <?php
 
 header('content-type:text/html; charset=utf-8');
-$first = "Hello";
-$second="1st";
-$third="World";
-
-echo "<p id='a'>".$first."</p><p id='b'>".$second."</p><p id='c'>".$third."</p>";
-echo "i'v gone to the store."." ";
-echo '"text"';
-
-//first value
-$role="student";
-$course="course 1";
-$duration=4;
-$user="Mamba";
-$txt="Hi $user you are .... as a $role in $course for $duration";
-echo "<p id='value'>".$txt."</p>";
-<<<<<<< HEAD
-asdasd
-=======
-
-//second value
-$role="lector";
-$course="course 2";
-$duration=5;
-$user="Samba";
-$txt="Hi $user you are .... as a $role in $course for $duration";
-echo "<p id='value2'>".$txt."</p>";
-
->>>>>>> origin/master
 
 
 ?>
