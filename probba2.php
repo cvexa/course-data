ee0ca2d883685c12afdd92385f98ef67978bdbe9
cvexa <cvexxxxa@abv.bg>
2016-01-27 18:42:30 +0200

    test2

--- probba2.php
+++ probba2.php
@@ -2,24 +2,8 @@
 <head>
 	<title>Probba php</title>
 	<style type="text/css">
-	#a{
-		color: red;
-	}
-	#b{
-		color: blue;
-	}
-	#c{
-		color: green;
-	}
 	body{
 		background-color: grey;
-	}
-	#value{
-		font-size: 22px;
-	}
-	#value2{
-		font-size: 27px;
-		color:red;
 	}
 	</style>
 </head>
