de443a337a5992586949d5047eaf2aedb95706b2
cvexa <cvexxxxa@abv.bg>
2016-01-27 19:18:40 +0200

    test2

--- probba2.php
+++ probba2.php
@@ -13,6 +13,19 @@
 <?php
 
 header('content-type:text/html; charset=utf-8');
+$a=rand(0,100);
+
+if ($a % 2 ==0){
+	echo '$a ='.$a." even";
+	if ($a === 0){
+		echo "$a = 0";
+	}
+}elseif ($a % 2 != 0) {
+	echo '$a ='.$a." Odd";
+}else{
+	echo '$a=' .$a. ' $a = 0';
+}
+
 
 
 ?>
