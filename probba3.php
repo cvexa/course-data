748f91d66bb76323cd315725193f057849f0cd1e
cvexa <cvexxxxa@abv.bg>
2016-01-31 14:36:28 +0200

    test3

---
+++ probba3.php
@@ -0,0 +1,24 @@
+<!html>
+<head>
+	<title>Probba3 php</title>
+	<style type="text/css">
+	body{
+		background-color:;
+	}
+	</style>
+</head>
+<body>
+
+
+<?php
+
+header('content-type:text/html; charset=utf-8');
+
+
+
+
+//newtask
+
+?>
+</body>
+</html>
