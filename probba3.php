021dba00b7b0efc16b61e8b62a4394f2948f2ed3
cvexa <cvexxxxa@abv.bg>
2016-01-31 15:53:19 +0200

    test

--- probba3.php
+++ probba3.php
@@ -16,11 +16,15 @@
 
 header('content-type:text/html; charset=utf-8');
 
-echo "<form action='probba3.php' method='get'>";
+echo "<form action='probba3.php' method='post'>";
 echo "<input type='text' name='username' placeholder='username'>";
 echo "<p><input type='password' name='password' placeholder='password'></p>";
 echo "<input type='submit' name='submit' value='register'>";
 echo "</form>";
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>" ;
 
 ?>
 </body>
