5790aa99c0114a8ff83adfbb67e628142eea7d0d
cvexa <cvexxxxa@abv.bg>
2016-01-31 15:01:27 +0200

    test

--- probba3.php
+++ probba3.php
@@ -15,9 +15,16 @@
 header('content-type:text/html; charset=utf-8');
 
 
+$user_data = array('first_name'=> "Veni" ,
+	               'second_name'=> "Ivanov",
+	               'third_name'=> "Georgiev",
+	               'age'       => "20",
+	               'profession'=>'director');
 
-
-//newtask
+echo "<ul>";
+echo "<li>First name - ".$user_data['first_name']."</li>"; 
+echo "<li>Second name - ".$user_data['second_name']."</li>";
+echo "</ul>";
 
 ?>
 </body>
