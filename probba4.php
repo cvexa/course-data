504c61a9a2ccbd89f059f6ee31fd5033c98e36fb
cvexa <c_v_e_x_a@abv.bg>
2016-02-04 19:33:41 +0200

    test4

--- probba4.php
+++ probba4.php
@@ -14,7 +14,13 @@
 <?php
 
 //firsttask
- 
+ $phone = array('Ivan' => '+0892346789' ,
+                'Petur' => '+0872346755',
+                'Maks' => '+0872333755' );
+
+foreach ($phone as $key => $value) {
+	echo '<p>'.$key.'- '.$value.'</p>';
+}
 
 
 
