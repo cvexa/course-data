f3dc61e5298052ad9404061b330c5605261e861b
cvexa <c_v_e_x_a@abv.bg>
2016-02-04 19:50:47 +0200

    test4

--- probba4.php
+++ probba4.php
@@ -14,13 +14,18 @@
 <?php
 
 //firsttask
- $phone = array('Ivan' => '+0892346789' ,
-                'Petur' => '+0872346755',
-                'Maks' => '+0872333755' );
-
-foreach ($phone as $key => $value) {
-	echo '<p>'.$key.'- '.$value.'</p>';
+ $arr = array('Холандия' => 'Амстердам' ,
+               'България' => 'София',
+               'Франция' => 'Париж', );
+echo "<ol type='i'>";
+foreach ($arr as $key => $value) {
+	
+	echo "<li>Столицата на $key е $value</li>";
+		
 }
+echo "</ol>";
+
+
 
 
 
