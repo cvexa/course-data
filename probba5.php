f00478b9f00f108e3ced98de08dba26c444f901d
cvexa <c_v_e_x_a@abv.bg>
2016-02-07 14:35:26 +0200

    test5

---
+++ probba5.php
@@ -0,0 +1,37 @@
+<?php header('content-type:text/html; charset=utf-8');?>
+<!html>
+<head>
+	<title>Probba5 php</title>
+	<style type="text/css">
+	body{
+		text-align: ;
+	}
+	</style>
+</head>
+
+<?php
+
+$a=1;
+$max=100;
+
+echo "<ul><li>";
+while ($a <= $max) {
+	echo $a." ";
+	
+	if (($a+1) % 10 == 0) {
+		echo "</li><li>";
+	}
+	$a+=2;
+}
+echo "</li></ul>";
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
