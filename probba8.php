7b571b9676f685aab6250d5c11c6ff13e7dccaa5
cvexa <c_v_e_x_a@abv.bg>
2016-02-18 18:43:14 +0200

    matrix_masive

---
+++ probba8.php
@@ -0,0 +1,18 @@
+<?php header('content-type:text/html; charset=utf-8');?>
+<!html>
+<head>
+	<title>Probba8 php</title>
+	<style type="text/css">
+	body{
+		text-align: ;
+	}
+	</style>
+</head>
+
+<?php
+
+
+
+?>
+</body>
+</html>
