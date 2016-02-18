c6443c9282b83d415ed2a118174b139c5174a876
cvexa <c_v_e_x_a@abv.bg>
2016-02-18 19:38:01 +0200

    matrix

--- probba8.php
+++ probba8.php
@@ -10,13 +10,31 @@
 </head>
 
 <?php
-$cars=array(array('brand' =>'vw' ,'model' =>'polo','color'=>'grey','price'=>'25.000' ),
-            array('brand' =>'skoda' ,'model' =>'yeti','color'=>'white','price'=>'33.000' ),
-            array('brand' =>'mazda' ,'model' =>'rx8','color'=>'green','price'=>'55.000' ),
-            array('brand' =>'opel' ,'model' =>'kadet','color'=>'blue','price'=>'100.000' ),
+$cars=array(array('brand' =>'vw' ,'model' =>'polo','color'=>'grey','price'=>'25.000','sold'=>'100' ),
+            array('brand' =>'skoda' ,'model' =>'yeti','color'=>'white','price'=>'33.000','sold'=>'150' ),
+            array('brand' =>'mazda' ,'model' =>'rx8','color'=>'green','price'=>'55.000','sold'=>'200' ),
+            array('brand' =>'opel' ,'model' =>'kadet','color'=>'blue','price'=>'100.000','sold'=>'250' ),
             );
 
-
+$count=count($cars);
+echo '<table border="1">';
+echo "<tr>";
+echo "
+<td>brand</td>
+<td>model</td>
+<td>color</td>
+<td>price</td>
+<td>sold</td>
+";
+echo "</tr>";
+for ($i=0; $i < $count; $i++) { 
+	echo "<tr>";
+	foreach ($cars[$i] as $value) {
+		echo "<td>".$value."</td>";
+	}
+	echo "</tr>";
+}
+echo '</table>';
 ?>
 </body>
 </html>
