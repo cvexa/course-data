8e687038adc25bfb34e256512e33c6d67dd77b9d
cvexa <c_v_e_x_a@abv.bg>
2016-02-21 16:04:44 +0200

    function

--- probba9.php
+++ probba9.php
@@ -20,6 +20,34 @@ print_calc($c,$d);
 print_calc($f,$g);
 
 
+function math($m,$n,$param1, $param2){
+	echo $m;
+	if ($param1 < $param2) {
+	for ($k=$param1; $k <= $param2 ; $k++) { 
+	echo $k." ";
+	}
+}elseif ($param1 > $param2) {
+for ($i=$param1; $i >= $param2 ; $i--) { 
+	echo $i." ";
+    }
+    } 
+
+
+	echo $n;
+}
+$tag1='<p>';
+$tag2='</p>';
+$param1=42;
+$param2=3;
+$tagg='<b>';
+$tagg2='</b>';
+
+
+math($tag1,$tag2,$param1,$param2);
+math('<h1>','</h1>',$param1,$param2);
+math($tagg,$tagg2,$param1,$param2);
+
+
 
 
 
