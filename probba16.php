ceea1403f8a4d79720a7104a0e6fe44616a87502
cvexa <c_v_e_x_a@abv.bg>
2016-03-20 15:41:45 +0200

    tetetetet

---
+++ probba16.php
@@ -0,0 +1,47 @@
+<?php
+function big($a){
+	$res=mb_strtoupper($a);
+	echo $res;
+}
+
+function small($b){
+	$res=mb_strtolower($b);
+	echo $res;
+}
+function match($c,$find){
+   $res=preg_match($find,$c);
+   echo $find."is found".$res."times";
+}
+
+
+$b='как си днес бе, момче';
+$d='how are you today';
+$e='how';
+
+echo "големи - ";
+big($b);
+echo "<p>малки - ";
+small('ДОБРЕ ДЕ ДОБРЕ,ДОБРЕ');
+echo "</p>";
+if (empty($_GET['check'])) {
+ 	
+
+echo "<p>Check if some word is in entered text bellow:";
+echo "<form action='probba16.php' method='get'>";
+echo "<p>Enter the text</p>";
+echo "<input type='text' name='check' placeholder='enter some text...'>";
+echo "<p>Enter the word you want to check if is there : </p>";
+echo "<p><input type='text' name='check_word' placeholder='enter the word...'></p>";
+echo "<input type='submit' name='submit' value='go'>";
+echo "</form>";
+}else{
+	$word=$_GET['check_word'];
+match($_GET['check'],"/$word/");
+echo "</p>"; 
+}
+
+
+
+
+
+?>
