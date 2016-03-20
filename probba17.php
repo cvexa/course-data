0eb74912d1c86033c51edc2da01faa054539b16c
cvexa <c_v_e_x_a@abv.bg>
2016-03-20 16:30:33 +0200

    test

--- probba17.php
+++ probba17.php
@@ -1,4 +1,46 @@
 <?php
+function convert(){
+	for ($i=chr(65); $i <chr(90) ; $i++) { 
+		echo $i;
+	}
+}
 
+function convert2(){
+	for ($k=chr(97); $k <chr(122) ; $k++) { 
+		echo $k;
+	}
+}
+
+function numbers(){
+   for ($m=1; $m <9; $m++) { 
+   	echo $m;
+   }
+}
+function pass($pass){
+	$new=md5($pass);
+	echo $new;
+}
+
+
+
+convert();
+echo "<pre>";
+numbers();
+echo "</pre>";
+convert2();
+
+
+
+if (empty($_POST['submit'])) {
+echo "<form action='probba17.php' method='post'>";
+echo "<p>user name</p>";
+echo "<input type='text' name='username' placeholder='enter some name...'>";
+echo "<p>password </p>";
+echo "<p><input type='password' name='password' placeholder='enter the pss...'></p>";
+echo "<input type='submit' name='submit' value='go'>";
+echo "</form>";
+}else{
+pass($_POST['password']);   
+}
 
 ?>
